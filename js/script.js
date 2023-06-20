7dbfdc7a1c0caef9865cf808299117bddc78b245
Alan Martinez <pharos199@gmail.com>
2023-06-19 19:26:13 -0700

    updating all new files to richie branch

--- js/script.js
+++ js/script.js
@@ -53,6 +53,29 @@ videoBtn.forEach(btn =>{
     });
 });
 
+var swiper1 = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  on: {
+    autoplayTimeLeft(s, time, progress) {
+      progressCircle.style.setProperty("--progress", 1 - progress);
+      progressContent.textContent = ${Math.ceil(time / 1000)}s;
+    }
+  }
+});
+
 var swiper = new Swiper(".review-slider", {
     spaceBetween: 20,
     grabCursor:true,
